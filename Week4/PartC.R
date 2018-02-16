587ee2d6a3643fab363ac901aba7ef3241f0da72
Hussein Ahmed <huahme@microsoft.com>
2018-02-16 09:09:43 -0800

    add part C

--- Week4/PartC.R
+++ Week4/PartC.R
@@ -4,8 +4,9 @@ m_data <- read.csv("cifar-10-batches-bin/allData.csv", header=TRUE)
 v_labels <- m_data[,1]
 m_features <- m_data[,-1]
 
-m_meanImages <- matrix(0,10,3072)
+#m_meanImages <- matrix(0,10,3072)
 m_distances <- matrix(0,10,10)
+pcas <- vector("list", 10)
 
 #calculating errors for 10 categories
 for(i in 1:10)
@@ -13,8 +14,10 @@ for(i in 1:10)
   print(c("starting category numer", i))
   v_positions <-  v_labels == i - 1
   m_categoryFeatures <- m_features[v_positions,]
-  #print()
-  m_meanImages[i,] <- apply(m_categoryFeatures, c(2), mean)
+  temp <- princomp(m_categoryFeatures) 
+  temp$scores[,21:3072] <- 0 
+  pcas[[i]] <- temp
+  #m_meanImages[i,] <- apply(m_categoryFeatures, c(2), mean)
 }
 
 print("Calculate Distance")
@@ -23,7 +26,11 @@ for(i in 1:9)
   for(j in (i+1):10)
   {
     print(c(i,"and",j))
-    m_distances[i,j] <- sqrt(sum((m_meanImages[i,]  - m_meanImages[j,]) ^ 2))
+    irestoration <- t(pcas[[j]]$loadings %*% t(pcas[[j]]$scores) + pcas[[i]]$center)
+    jrestoration <- t(pcas[[i]]$loadings %*% t(pcas[[i]]$scores) + pcas[[j]]$center)
+    iError <- sqrt(sum((irestoration  - pcas[[i]]$center) ^ 2))
+    jError <- sqrt(sum((jrestoration  - pcas[[j]]$center) ^ 2))
+    m_distances[i,j] <- (iError + jError) / 2
   }
 }
 
@@ -37,4 +44,3 @@ ggplot(data=mds.data, aes(x=X, y=Y, label=as.character(v_catnames[,]))) +
   ylab("Dimension 2") +
   ggtitle("Distance Between Categories")
 
-#barplot(v_errors, main="Reconstruction Error Per Category",ylab="Error", xlab="Categories", names.arg=v_catnames[,])
