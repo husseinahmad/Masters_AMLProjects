247c058bbc652b4a4b802a37a3828316819317c2
Hussein Ahmed <huahme@microsoft.com>
2018-04-12 20:34:59 -0700

    add main implementation problem1

--- Week11/Problem1.R
+++ Week11/Problem1.R
@@ -83,6 +83,30 @@ calculateXSum <- function(m_image, s_xPos, s_yPos, s_xTheta){
   return(m_image[s_xPos, s_yPos] * s_xTheta)
 }
 
+calculateEPHX_HiddenSum <- function(m_pis, s_xPos, s_yPos, s_hiddenTheta){
+  s_pixelEQ <- (2 * m_pis[s_xPos, s_yPos]) - 1 
+  s_hiddenSum <- 0
+  #left node
+  if(s_xPos > 1)
+    s_hiddenSum <- s_hiddenSum + (s_hiddenTheta * ( (2 * m_pis[s_xPos - 1, s_yPos]) - 1 ) * s_pixelEQ)
+  #right node
+  if(s_xPos < 28)
+    s_hiddenSum <- s_hiddenSum + (s_hiddenTheta * ( (2 * m_pis[s_xPos + 1, s_yPos]) - 1 ) * s_pixelEQ)
+  #up node
+  if(s_yPos > 1)
+    s_hiddenSum <- s_hiddenSum + (s_hiddenTheta * ( (2 * m_pis[s_xPos, s_yPos - 1]) - 1 ) * s_pixelEQ)
+  #down node
+  if(s_yPos < 28)
+    s_hiddenSum <- s_hiddenSum + (s_hiddenTheta * ( (2 * m_pis[s_xPos, s_yPos + 1]) - 1 ) * s_pixelEQ)
+  
+  return(s_hiddenSum)
+}
+
+calculateEPHX_XSum <- function(m_pis, m_image, s_xPos, s_yPos, s_xTheta){
+  s_pixelEQ <- (2 * m_pis[s_xPos, s_yPos]) - 1 
+  return(m_image[s_xPos, s_yPos] * s_xTheta * s_pixelEQ)
+}
+
 updatePIs <- function(m_image, m_updateOrder, m_pis){
   s_hiddenTheta <- 0.8
   s_xTheta <- 2
@@ -120,14 +144,30 @@ for(i in 1:20)
   l_pis[[i]] <- m_pis
 }
 
+v_energy <- vector("numeric", 20)
+s_epsilon <- 10 ^ -10
+s_hiddenTheta <- 0.8
+s_xTheta <- 2
+m_variationalEnergy <- matrix(0, 20, 10)
+
+
 for(iteration in 1:10)
 {
-  #calculate Q
-  
-  # update pis
-  for(i in 1:20)
+  for(image_index in 1:20)
   {
-    l_pis[[i]] <- updatePIs(m_noisedImages[[i]], l_updateOrders, l_pis[[i]])
+    #calculate Q
+    s_eqLogQ <- sum(sapply(unlist(l_pis[[image_index]]), function(x) (x*log(x + s_epsilon)) + ((1-x)*log((1-x) + s_epsilon))  ))
+    s_eqLogPHX <- 0
+    for(r in 1:28){
+      for(c in 1:28){
+        s_eqLogPHX <- s_eqLogPHX + calculateEPHX_HiddenSum(l_pis[[image_index]], r, c, s_hiddenTheta) + calculateEPHX_XSum (l_pis[[image_index]], m_noisedImages[[image_index]], r, c, s_xTheta)
+      }
+    }
+    
+    m_variationalEnergy[image_index, iteration] <- s_eqLogQ - s_eqLogPHX
+  
+    # update pis
+    l_pis[[image_index]] <- updatePIs(m_noisedImages[[image_index]], l_updateOrders[[image_index]], l_pis[[image_index]])
   }
 }
 
